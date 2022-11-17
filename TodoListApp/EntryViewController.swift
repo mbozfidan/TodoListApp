b29cf2058369db0cb1d267f5be38b341e6f5153c
mbozfidan <mbozfidan@gmail.com>
2022-11-17 15:27:19 +0300

    TableViewOutlet and To do list entry

---
+++ TodoListApp/EntryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  EntryViewController.swift
+//  TodoListApp
+//
+//  Created by Mehmet Buğra Özfidan on 17.11.2022.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var field: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        field.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveTask))
+
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        saveTask()
+        
+        return true
+    }
+    
+    @objc func saveTask() {
+        
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        
+    }
+    
+
+}
---
+++ TodoListApp/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  TodoListApp
+//
+//  Created by Mehmet Buğra Özfidan on 17.11.2022.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
